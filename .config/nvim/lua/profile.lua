feee201f52f8f5f8697e361e2933b4fd93ef66b0
matt-clarson <clarson469@googlemail.com>
2022-04-25 22:05:44 +0100

    added go ls config

--- .config/nvim/lua/profile.lua
+++ .config/nvim/lua/profile.lua
@@ -54,7 +54,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'tsserver' }
+local servers = { 'gopls', 'tsserver' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
