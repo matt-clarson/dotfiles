44c971b6a7ab579ec4268fcd566a107b683e7f93
Matthew Clarson <matt@Matthews-MacBook-Air.local>
2024-07-07 12:21:24 +0100

    add todo search mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -637,7 +637,10 @@ require("lazy").setup({
 		"folke/todo-comments.nvim",
 		event = "VimEnter",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
+		config = function()
+			require("todo-comments").setup({ signs = false })
+			vim.keymap.set("n", "<leader>st", ":TodoTelescope<cr>", { desc = "[S]earch [T]odos" })
+		end,
 	},
 
 	{ -- Collection of various small independent plugins/modules
