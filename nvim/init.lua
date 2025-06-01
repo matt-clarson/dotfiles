1b4d8bd7364531543c30bb5a5f851e16e1c5cdd8
matt-clarson <clarson469@gmail.com>
2025-06-01 14:45:17 +0100

    fix svelte lsp file watcher

--- nvim/init.lua
+++ nvim/init.lua
@@ -353,6 +353,16 @@ require("lazy").setup({
 					--
 					-- When you move your cursor, the highlights will be cleared (the second autocommand).
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+					if client.name == "svelte" then
+						vim.api.nvim_create_autocmd("BufWritePost", {
+							pattern = { "*.js", "*.ts", "*.svelte" },
+							callback = function(ctx)
+								client:notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+							end,
+						})
+					end
+
 					if client and client.server_capabilities.documentHighlightProvider then
 						local highlight_augroup =
 							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
